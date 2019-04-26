13e978dc43ef75135abe25a431fd3e3df7082b00
LorenzoBellomo <lorenzo.bellomo.lb@gmail.com>
2019-04-26 09:38:01 +0200

    switch to ubuntu

--- Assignment3/assignment3.cpp
+++ Assignment3/assignment3.cpp
@@ -14,6 +14,18 @@ using board_t = cimg_library::CImg<unsigned char>;
 const unsigned int alive = 000;
 const unsigned int dead  = 255;
 
+board_t board;
+int current_iteration = 0, n, iter_num, nw;
+
+void thd_func(int id, int left_limit, int right_limit) {
+
+	for(int i = left_limit; i < right_limit; i++){
+		for(int j = 0; j < n; j++) {
+
+		}
+	}
+	
+}
 
 
 int main(int argc, char * argv[]) {
@@ -23,9 +35,9 @@ int main(int argc, char * argv[]) {
 		return(0);
 	}
 	// get matrix dimensions from the command line
-	int n = atoi(argv[1]);
-	int iter_num = atoi(argv[2]);
-	int nw = atoi(argv[3]);
+	n = atoi(argv[1]);
+	iter_num = atoi(argv[2]);
+	nw = atoi(argv[3]);
 	(argc == 5)? srand(atoi(argv[4])) : srand(time(NULL));
 
 	cout << "Using " << n << "x" << n << " board " << endl;
@@ -38,29 +50,41 @@ int main(int argc, char * argv[]) {
 	*/
 
 	// create an empty board
-	board_t board(n,n,1,1,0);
+	board(n,n,1,1,0);
+	vector<thread> threadPool;
 
 	for(int i=0; i<n; i++)
 		for(int j=0;  j<n; j++)
 			board(i,j,0,0) = (rand() % 32 == 0 ? alive : dead);
 
-	CImgDisplay main_displ(board,"Init");
+	CImgDisplay main_displ(board,"Game of Life");
 	sleep(2);  // show for two seconds
 
-	for(int p = 0; p < iter_num; p++) {
-		// change some individuals
-		for(int k=0; k<n*n/100; k++) {
-			int i = rand() % n;
-			int j = rand() % n;
-			if(board(i,j,0,0) == alive)
-				board(i,j,0,0) = dead;
-			else
-				board(i,j,0,0) = alive;
-		}
-		// and redisplay
-		main_displ.display(board);
-		sleep(1);
+	int range = num_tasks / nw;
+    int extra = num_tasks % nw;
+	int prev = 0;
+
+	for(int i = 0; i < extra; i++) {
+		threads.push_back(thread(
+			thd_func, 
+			prev,
+			prev + range + 1,
+		));
+		prev += (range + 1);
 	}
+        
+	for(int i = 0; i < nw - extra; i++) {            
+		threads.push_back(thread(
+			thd_func, 
+			prev,
+			prev + range,
+		));
+		prev += range;
+	}
+
+	for(thread& t: threadPool)
+        t.join();
+
 	return(0);
   
 }

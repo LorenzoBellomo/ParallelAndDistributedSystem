ed20bfc6e9cddfef9be0a636c19cbaf015ae2a26
LorenzoBellomo <lorenzo.bellomo.lb@gmail.com>
2019-07-22 17:42:09 +0200

    refactor interaction between contiguous supersteps

--- FinalProject/src/posixBSP.hpp
+++ FinalProject/src/posixBSP.hpp
@@ -15,27 +15,40 @@ template <typename T>
 class posixBSP : public BSP<T> {
 
 private:
-    vector<super_step<T>> superSteps;
-    int nw;
+    vector<super_step<T>> super_steps;
+    int nw, current_ss;
     typename vector<T>::iterator begin, end;
     
 public:
 
     posixBSP(
-        vector<function<void(void)>> ss, 
+        vector<function<void(void)>*> ss, 
         int num_workers, 
         typename vector<T>::iterator _begin,
         typename vector<T>::iterator _end): 
-            nw(num_workers), begin(_begin), end(_end) {
+            nw(num_workers), current_ss(0), begin(_begin), end(_end) 
+    {
+        super_steps = vector<super_step<T>>();
+        for(int i = 0; i < ss; i++) 
+            super_steps.push_back(ss[i], this, nw, i);
 
     }
 
-    void send_to(
+    void send_coll_to(
         typename vector<T>::iterator _begin, 
         typename vector<T>::iterator _end,
-        int ss_lvl_index,
-        int worker_index)   {
+        int worker_index)   
+    {
+        super_steps[current_ss+1].offer_tasks(_begin, _end, worker_index);
          
+    }
+
+    void send_to(T elem, int worker_index) {
+        super_steps[current_ss+1].offer_task(elem, worker_index);
+    }
+
+    void sync(int from) {
+        super_steps[current_ss].join_barrier();
     }
 };
 
--- FinalProject/src/superStep.hpp
+++ FinalProject/src/superStep.hpp
@@ -4,26 +4,56 @@
 
 #include <iostream>
 #include <functional>
+#include <thread>
 
 #include <safeQueue.hpp>
 #include <BSP.hpp>
+#include <barrier.hpp>
 
 using namespace std; 
 
 template <typename T>
 class super_step {
 private:
-    safe_queue<T> queue_in;
     function<void(void)> ss_func;
-    BSP<T> bsp;
+    BSP<T> *bsp;
+    vector<thread> thds;
+    vector<safe_queue<T>> in_queues;
+    barrier barr;
+    int nw, ss_lvl;
 
 public:
-    super_step(function<void(void)> func, BSP<T> _bsp): bsp(_bsp), ss_func(func) {
-        queue_in = safe_queue<T>();
+    super_step(function<void(void)> func, BSP<T> *_bsp, int _nw, int _ss_lvl): 
+        bsp(_bsp), ss_func(func), nw(_nw), ss_lvl(_ss_lvl)
+    {
+        barr = barrier();
+        in_queues = vector<safe_queue<T>>(nw);
     }
 
-    void offer_task(T task) {
-        queue_in.push(task);
+    void start_ss() {
+        for(int i = 0; i < nw; i++)
+            thds.push_back(func);
+    }
+
+    /*void end_ss() {
+        for(auto t : thds)
+            t.join();
+    }*/
+
+    void offer_task(T task, int worker_index) {
+        in_queues[worker_index].push(task);
+    }
+
+    void offer_tasks(
+        typename vector<T>::iterator begin, 
+        typename vector<T>::iterator end
+        int worker_index) 
+    {
+        in_queues[worker_index].push_multiple(begin, end);
+    }
+
+    void join_barrier() {
+        barr.barrier_wait();
     }
 
 };
